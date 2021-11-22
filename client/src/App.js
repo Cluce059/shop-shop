ae9d12a44ff30bd1de9f08db86179f7271ca973b
caroline <cluce059@gmail.com>
2021-11-21 20:23:15 -0500

    cleaned up traces of globalstate

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,10 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import { StoreProvider } from "./utils/GlobalState";
+//import { StoreProvider } from "./utils/GlobalState";
 import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
+//import { useDispatch, useSelector } from 'react-redux';
 
 const client = new ApolloClient({
   request: (operation) => {
